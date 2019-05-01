e63423ac92c8f14e09aac8d3e204ed04b3d7e8fc
Joe Jamsky <cba1067950@yahoo.com>
2019-05-01 03:04:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,8 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+:yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+  programmer_hash[:yukihiro_matsumoto =>
 end
 
 def changing_alan
